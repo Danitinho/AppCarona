0446b7eafd35776825dd642f268f9dc17921474e
Joao Victor dos Santos Rocha <joaovic.rocha3@gmail.com>
2018-06-18 16:13:32 -0400

    commit inicial

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav, List } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { ListPage } from '../pages/list/list'
+import { HomePage } from '../pages/home/home';
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  @ViewChild(Nav) nav: Nav;
+  rootPage:any = HomePage;
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    
+    //this.any = [{ title: 'List', component:ListPage }]
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      statusBar.styleDefault();
+      splashScreen.hide();
+    });
+  }
+}
+
---
+++ src/pages/home/home.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { ListPage } from "../list/list"
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+  goToList() {
+    this.navCtrl.push(ListPage);
+  }
+}
